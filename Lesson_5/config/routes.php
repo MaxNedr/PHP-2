6fc5e92691add899037d6bd24d2c5f8ad59b851b
NedrNout <maxim.nedozrelov@ya.ru>
2018-11-26 15:16:11 +0300

    дз не готово

---
+++ Lesson_5/config/routes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by Artem Manchenkov
+ * artyom@manchenkoff.me
+ * manchenkoff.me © 2018
+ */
+
+use app\controllers\HomeController;
+
+/**
+ * Настройки маршрутов приложения
+ */
+return [
+    'routes' => [
+        //'website/path' => ['ControllerClass', 'actionName'],
+
+        '/' => [HomeController::class, 'page'],
+        'home/{name}' => [HomeController::class, 'hello'],
+        'reg' => [HomeController::class, 'reg']
+    ]
+];
